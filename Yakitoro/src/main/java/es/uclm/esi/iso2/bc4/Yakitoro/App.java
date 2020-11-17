ac2cb04249f104df912c90fcbaae09a6b9a4e103
ArturoAlcaniz <Arturo.Alcaniz1@alu.uclm.es>
2020-11-17 11:57:30 +0100

    Configuracion Maven y comprobacion conexion database

---
+++ Yakitoro/src/main/java/es/uclm/esi/iso2/bc4/Yakitoro/App.java
@@ -0,0 +1,13 @@
+package es.uclm.esi.iso2.bc4.Yakitoro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
